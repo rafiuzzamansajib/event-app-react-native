29727c472ef3611d6496b5eef3c9c22b166983df
rafiuzzamansajib <mdrafiuzzamansajib@gmail.com>
2021-12-16 00:44:33 +0600

    add packages and all js file

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as Font from 'expo-font';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  StatusBar,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Tabs from './navigation/Tabs';
+import { Featured, EventDetail } from './screens';
+import { customFonts } from './constants';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [assetsLoaded, setAssetLoaded] = useState(false);
+
+  /* Loading custom fonts in async */
+  const _loadAssetsAsync = async () => {
+    await Font.loadAsync(customFonts);
+    setAssetLoaded(true);
+  };
+
+  useEffect(() => {
+    _loadAssetsAsync();
+  });
+
+  return assetsLoaded ? (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content"></StatusBar>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Featured"
+      >
+        <Stack.Screen name="Featured" component={Tabs} />
+        <Stack.Screen name="EventDetail" component={EventDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ) : (
+    <ActivityIndicator size="small"></ActivityIndicator>
   );
 }
 
